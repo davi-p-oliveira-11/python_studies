f5e2ab8537c00c7b05fc71cdea45d391c88b25fa
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-31 14:54:16 -0300

    updated decorators 2

---
+++ decorators_iterators_generator/decorator_with_args 2.py
@@ -0,0 +1,18 @@
+def my_decorator(function):
+  def envelope(*args, **kwargs):
+    print("do something before execution")
+    result = function(*args, **kwargs)
+    print("do after the execution")
+    return result
+
+  return envelope
+
+@my_decorator
+def hello_world(name, other_arg):
+  print(f"Hello World {name}!")
+  return name.upper()
+
+
+result = hello_world("John", 1000)
+print(result)
+
