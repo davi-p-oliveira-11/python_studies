49f3cd25559126b04793fc6bb63b5c67fb2c8766
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-01 12:13:03 -0300

    upated 1_datetime.py

---
+++ date-hours/1_datetime.py
@@ -0,0 +1,11 @@
+from datetime import date, datetime, time
+today_date = date(2025, 1, 4)
+print(date)
+print(date.today())
+
+date_hour = datetime(2025, 1, 4, 12, 30, 20)
+print(date_hour)
+print(datetime.today())
+
+hour = time(19, 20, 0)
+print(hour)
